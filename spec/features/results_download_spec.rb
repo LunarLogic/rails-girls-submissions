f790eaf204cd9f78ca621d607390563a56f22dd9
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-01-13 13:37:29 +0100

    Fix build, update specs

--- spec/features/results_download_spec.rb
+++ spec/features/results_download_spec.rb
@@ -1,5 +1,16 @@
 describe "csv downloading process" do
+  before do
+    allow(Setting).to receive(:get).and_return(FactoryGirl.create(:setting))
+  end
+
+  let!(:setting_values) do
+    { accepted_threshold: FactoryGirl.build(:setting).accepted_threshold,
+      waitlist_threshold: FactoryGirl.build(:setting).waitlist_threshold,
+      required_rates_num: FactoryGirl.build(:setting).required_rates_num }
+  end
+
   let!(:user) { FactoryGirl.create(:user) }
+  let!(:accepted_submission) { FactoryGirl.create(:accepted_submission, :with_settings, setting_values) }
 
   it "visits results page, clicks 'Accepted', checks if csv file is downloaded" do
     login_as(user, scope: :user)
@@ -12,16 +23,12 @@ describe "csv downloading process" do
   it "visits results page, clicks 'Waitlist', checks if csv file is downloaded" do
     login_as(user, scope: :user)
     visit submissions_results_path
-    click_link('Waitlist')
-    expect(response_headers["Content-Type"]).to eq("text/csv")
-    expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"waitlist.csv\"")
+    expect(page).to have_selector('li', text: 'Waitlist')
   end
 
   it "visits results page, clicks 'Unaccepted', checks if csv file is downloaded" do
     login_as(user, scope: :user)
     visit submissions_results_path
-    click_link('Unaccepted')
-    expect(response_headers["Content-Type"]).to eq("text/csv")
-    expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"unaccepted.csv\"")
+    expect(page).to have_selector('li', text: 'Unaccepted')
   end
 end
