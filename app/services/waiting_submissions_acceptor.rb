7357b98dac6004ee6ea598e75acd60e1ba3a8504
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-01-16 15:03:25 +0100

    Extract logic from rake task to service class

---
+++ app/services/waiting_submissions_acceptor.rb
@@ -0,0 +1,35 @@
+class WaitingSubmissionsAcceptor
+  def self.call
+    #TODO: Confirm if submissions first in waiting line come first
+    waiting_submissions.each do |waiting_submission|
+      expired_submission = not_rejected_expired_submission
+      break unless expired_submission
+
+      reject_expired_submission(expired_submission)
+
+      send_email_with_confirmation_link(waiting_submission)
+    end
+  end
+
+  private
+
+  def waiting_submissions
+    SubmissionRepository.new.waitlist.select { |w| w.confirmation_token.nil? }
+  end
+
+  def expired_submission
+    Submission.all.select do |s|
+      s.confirmation_token.present? && s.confirmation_token_created_at < 1.week.ago && !s.rejected
+    end.first #TODO: Sort by confirmation_token_created_at?
+  end
+
+  def reject_expired_submission
+    expired_submission.reject
+    expired_submission.save
+  end
+
+  def send_email_with_confirmation_link
+    waiting_submission.generate_confirmation_token!
+    ResultsMailer.accepted_email(waiting_submission).deliver_now
+  end
+end
