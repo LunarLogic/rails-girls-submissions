2a1a290f0c3afa23970cc664e0aada436c384c0c
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-01-16 15:28:45 +0100

    Fix broken code

--- app/services/waiting_submissions_acceptor.rb
+++ app/services/waiting_submissions_acceptor.rb
@@ -17,18 +17,18 @@ class WaitingSubmissionsAcceptor
     SubmissionRepository.new.waitlist.select { |w| w.confirmation_token.nil? }
   end
 
-  def expired_submission
+  def not_rejected_expired_submission
     Submission.all.select do |s|
       s.confirmation_token.present? && s.confirmation_token_created_at < 1.week.ago && !s.rejected
     end.first #TODO: Sort by confirmation_token_created_at?
   end
 
-  def reject_expired_submission
+  def reject_expired_submission(expired_submission)
     expired_submission.reject
     expired_submission.save
   end
 
-  def send_email_with_confirmation_link
+  def send_email_with_confirmation_link(waiting_submission)
     waiting_submission.generate_confirmation_token!
     ResultsMailer.accepted_email(waiting_submission).deliver_now
   end
