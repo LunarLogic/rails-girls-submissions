dc08facac3136df37cd33de44bb0eef43bccdca2
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-01-17 14:34:05 +0100

    Don't count confirmed submissions as expired

--- app/services/waiting_submissions_acceptor.rb
+++ app/services/waiting_submissions_acceptor.rb
@@ -18,8 +18,8 @@ class WaitingSubmissionsAcceptor
   end
 
   def not_rejected_expired_submission
-    Submission.all.select do |s|
-      s.confirmation_token.present? && s.confirmation_token_created_at < 1.week.ago && !s.rejected
+    Submission.all.select do |submission|
+      expired_submission?(submission)
     end.first #TODO: Sort by confirmation_token_created_at?
   end
 
@@ -31,5 +31,9 @@ class WaitingSubmissionsAcceptor
   def send_email_with_confirmation_link(waiting_submission)
     waiting_submission.generate_confirmation_token!
     ResultsMailer.accepted_email(waiting_submission).deliver_now
+  end
+
+  def expired_submission?(submission)
+    submission.confirmation_token.present? && submission.confirmation_token_created_at < 1.week.ago && !submission.rejected && !submission.confirmed
   end
 end
--- spec/services/waiting_submissions_acceptor_spec.rb
+++ spec/services/waiting_submissions_acceptor_spec.rb
@@ -11,15 +11,21 @@ RSpec.describe WaitingSubmissionsAcceptor do
         waitlist_threshold: FactoryGirl.build(:setting).waitlist_threshold,
         required_rates_num: FactoryGirl.build(:setting).required_rates_num }
     end
-    let!(:waitlist_submission) { FactoryGirl.create(:waitlist_submission, :with_settings, setting_values) }
-    let!(:submission) do
+    let!(:waitlist_submission1) { FactoryGirl.create(:waitlist_submission, :with_settings, setting_values) }
+    let!(:waitlist_submission2) { FactoryGirl.create(:waitlist_submission, :with_settings, setting_values) }
+    let!(:expired_submission1) do
       FactoryGirl.create(:submission, confirmation_token: 'dsa', confirmation_token_created_at: 1.month.ago)
+    end
+    let!(:expired_submission2) do
+      FactoryGirl.create(:submission, confirmation_token: 'dsa', confirmation_token_created_at: 1.month.ago, confirmed: true)
     end
 
     it "sends email to waitlist submissions" do
       described_class.new.call
-      waitlist_submission.reload
-      expect(waitlist_submission.confirmation_token).not_to be_nil
+      waitlist_submission1.reload
+      waitlist_submission2.reload
+      expect(waitlist_submission1.confirmation_token).not_to be_nil
+      expect(waitlist_submission2.confirmation_token).to be_nil
       expect(ActionMailer::Base.deliveries.count).to eq(1)
     end
   end
