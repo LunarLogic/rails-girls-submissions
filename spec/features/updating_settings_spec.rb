2348537e0abb3795d396cc4232a7e77d144dfc85
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-01-20 13:38:45 +0100

    [104] Add numericality validation to thresholds

--- spec/features/updating_settings_spec.rb
+++ spec/features/updating_settings_spec.rb
@@ -1,20 +1,23 @@
+require "rails_helper"
+
 describe "testing updating settings:" do
-
-  let!(:user) { FactoryGirl.create(:user) }
-
-  it "moves to settings view, fills in the form fields, clicks save, checks if settings got updated" do
-    login_as(user, scope: :user)
+  before do
+    FactoryGirl.create(:setting)
+    login_as(FactoryGirl.create(:user), scope: :user)
     visit settings_path
-    fill_in 'Accepted threshold', with: 6
-    fill_in 'Waitlist threshold', with: 6
-    fill_in 'Required rates num', with: 6
+    fill_in 'Accepted threshold', with: 5
+    fill_in 'Waitlist threshold', with: 5
+    fill_in 'Required rates num', with: 5
     fill_in 'Beginning of preparation period', with: "2016/06/21"
     fill_in 'Beginning of registration period', with: "2016/06/22"
     fill_in 'Beginning of closed period', with: "2016/06/23"
     click_button "Save settings"
-    expect(Setting.get.accepted_threshold).to eq(6)
-    expect(Setting.get.waitlist_threshold).to eq(6)
-    expect(Setting.get.required_rates_num).to eq(6)
+  end
+
+  it "moves to settings view, fills in the form fields, clicks save, checks if settings got updated" do
+    expect(Setting.get.accepted_threshold).to eq(5)
+    expect(Setting.get.waitlist_threshold).to eq(5)
+    expect(Setting.get.required_rates_num).to eq(5)
     expect(Setting.get.beginning_of_preparation_period).to eq("Thu, 21 Jun 2016 00:00:00 CEST +02:00")
     expect(Setting.get.beginning_of_registration_period).to eq("Thu, 22 Jun 2016 00:00:00 CEST +02:00")
     expect(Setting.get.beginning_of_closed_period).to eq("Thu, 23 Jun 2016 00:00:00 CEST +02:00")
