df5bf2e073b57dfcc16a3fe7295ad0127a018105
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-01-26 16:38:08 +0100

    Add results download specs

--- spec/features/results_download_spec.rb
+++ spec/features/results_download_spec.rb
@@ -3,20 +3,41 @@ require 'rails_helper'
 describe "csv downloading process" do
   let!(:user)    { FactoryGirl.create(:user) }
   let!(:setting) { FactoryGirl.create(:setting, available_spots: 1) }
-  let!(:accepted_submissiom) { FactoryGirl.create(:submission, :with_rates,
-    rates_num: setting.required_rates_num) }
 
-  it "visits results page, clicks 'Accepted', checks if csv file is downloaded" do
-    login_as(user, scope: :user)
-    visit submissions_results_path
-    click_link('Accepted')
-    expect(response_headers["Content-Type"]).to eq("text/csv")
-    expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"accepted.csv\"")
+  context "when submissions exist" do
+    let!(:accepted_submission) { FactoryGirl.create(:submission, :with_rates,
+      rates_num: setting.required_rates_num) }
+    let!(:waitlist_submission) { FactoryGirl.create(:submission, :with_rates,
+      rates_num: setting.required_rates_num) }
+
+    it "visits results page, clicks 'Accepted', checks if csv file is downloaded" do
+      login_as(user, scope: :user)
+      visit submissions_results_path
+      click_link('Accepted')
+      expect(response_headers["Content-Type"]).to eq("text/csv")
+      expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"accepted.csv\"")
+    end
+
+    it "visits results page, clicks 'Waitlist', checks if csv file is downloaded" do
+      login_as(user, scope: :user)
+      visit submissions_results_path
+      click_link('Accepted')
+      expect(response_headers["Content-Type"]).to eq("text/csv")
+      expect(response_headers["Content-Disposition"]).to eq("attachment; filename=\"accepted.csv\"")
+    end
   end
 
-  it "visits results page, clicks 'Waitlist', checks if csv file is downloaded" do
-    login_as(user, scope: :user)
-    visit submissions_results_path
-    expect(page).to have_selector('li', text: 'Waitlist')
+  context "when submissions don't exist" do
+    it "Accepted download link is inactive" do
+      login_as(user, scope: :user)
+      visit submissions_results_path
+      expect(page).to have_selector('li', text: 'Accepted')
+    end
+
+    it "Waitlist download link is inactive" do
+      login_as(user, scope: :user)
+      visit submissions_results_path
+      expect(page).to have_selector('li', text: 'Waitlist')
+    end
   end
 end
