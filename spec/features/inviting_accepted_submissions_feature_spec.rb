6e2fd01c7897af5145444603206fbb13252b9808
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-01-31 15:18:05 +0100

    Test confirmation link in invitation email

---
+++ spec/features/inviting_accepted_submissions_feature_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'inviting accepted submissions' do
+  let(:setting) { FactoryGirl.build(:setting, available_spots: 2) }
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    ActionMailer::Base.deliveries.clear
+    allow(Setting).to receive(:get).and_return(setting)
+  end
+
+  it 'sends one email to every accepted submission' do
+    accepted_submissions = FactoryGirl.create_list(
+      :submission, 2,
+      :with_rates,
+      confirmation_status: 'not_avaible',
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    login_as(user, scope: :user)
+    visit submissions_results_path
+    click_link('Send')
+    click_link('Send')
+    expect(accepted_submissions[0].reload.confirmation_status).to eq 'awaiting'
+    expect(accepted_submissions[1].reload.confirmation_status).to eq 'awaiting'
+    expect(ActionMailer::Base.deliveries.count).to eq(2)
+  end
+
+  it 'confirms submission' do
+    invited_submission = FactoryGirl.create(
+      :submission,
+      :with_rates,
+      confirmation_status: 'awaiting',
+      confirmation_token: 'xxx',
+      confirmation_token_created_at: Time.now,
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    visit submissions_confirm_path(confirmation_token: invited_submission.confirmation_token)
+    expect(invited_submission.reload).to be_confirmed
+  end
+
+  it "doesn't confirm submission with expired confirmation token" do
+    invited_submission = FactoryGirl.create(
+      :submission,
+      :with_rates,
+      confirmation_status: 'awaiting',
+      confirmation_token: 'zzz',
+      confirmation_token_created_at: 1.week.ago - 1,
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    visit submissions_confirm_path(confirmation_token: invited_submission.confirmation_token)
+    expect(invited_submission.reload).not_to be_confirmed
+  end
+end
