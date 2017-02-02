1f8475f5398cf2cd430d6c96ecc5fde45c711121
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-02 14:49:24 +0100

    Use readers in SubmissionsExpirationHandler

--- app/services/submissions_expiration_handler.rb
+++ app/services/submissions_expiration_handler.rb
@@ -15,9 +15,10 @@ class SubmissionsExpirationHandler
   end
 
   private
+  attr_reader :submission_repository, :submissions_inviter
 
   def waitlist_submissions
-    @submission_repository.to_invite
+    submission_repository.to_invite
   end
 
   def submissions_to_be_expired
@@ -27,6 +28,6 @@ class SubmissionsExpirationHandler
   end
 
   def invite_submission(waitlist_submission)
-    @submissions_inviter.invite(waitlist_submission)
+    submissions_inviter.invite(waitlist_submission)
   end
 end
