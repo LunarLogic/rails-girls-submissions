7ab0c3ead9b4438ac2d8756b47fccce91b71c0bf
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-06 16:07:29 +0100

    Change rake task inviting new submissions in place of expired ones

--- lib/tasks/scheduled/handle_submissions_expiration.rake
+++
@@ -1,5 +0,0 @@
-namespace :scheduled do
-  task handle_submissions_expiration: :environment do
-    SubmissionsExpirationHandler.new.call
-  end
-end
--- spec/tasks/invite_new_submissions_in_place_of_expired_ones_spec.rb
+++ spec/tasks/invite_new_submissions_in_place_of_expired_ones_spec.rb
@@ -5,11 +5,11 @@
 require 'rails_helper'
 require 'rake'
 
-describe 'scheduled:handle_submissions_expiration' do
+describe 'scheduled:invite_new_submissions_in_place_of_expired_ones' do
   let(:rake)      { Rake::Application.new }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/tasks/scheduled/handle_submissions_expiration" }
-  let(:submissions_expiration_handler) { instance_double SubmissionsExpirationHandler }
+  let(:task_path) { "lib/tasks/scheduled/invite_new_submissions_in_place_of_expired_ones" }
+  let(:submisssions_inviter) { instance_double SubmissionsInviter }
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
@@ -24,8 +24,8 @@ describe 'scheduled:handle_submissions_expiration' do
   end
 
   it 'calls correct service' do
-    expect(SubmissionsExpirationHandler).to receive(:new).and_return(submissions_expiration_handler)
-    expect(submissions_expiration_handler).to receive(:call)
+    expect(SubmissionsInviter).to receive(:new).and_return(submisssions_inviter)
+    expect(submisssions_inviter).to receive(:call)
     subject.invoke
   end
 end
