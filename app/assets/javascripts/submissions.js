8a92dd1cb01e8e204defdeceee19718a1681c3a6
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-02-08 15:47:13 +0100

    [122] Quick fix]

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -1,6 +1,8 @@
 var ready = function() {
-  var rate = $('#user-rating').data('rate');
+  var rate = $('#js-user-rating').data('rate');
   document.getElementById("value_" + rate).checked = true;
-}
+};
 
-$(document).on('page:change', ready);
+if(window.location.pathname.match(/\/admin\/submissions\/\d*$/)) {
+  $(document).on('page:change', ready);
+}
