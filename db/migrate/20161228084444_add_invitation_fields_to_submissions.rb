55aeff3f579331fb38bd3f01949af35c1b74f3ac
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-08 18:41:04 +0100

    Add index for invitation_token

--- db/migrate/20161228084444_add_invitation_fields_to_submissions.rb
+++ db/migrate/20161228084444_add_invitation_fields_to_submissions.rb
@@ -2,5 +2,6 @@ class AddInvitationFieldsToSubmissions < ActiveRecord::Migration
   def change
     add_column :submissions, :invitation_token, :string
     add_column :submissions, :invitation_token_created_at, :datetime
+    add_index :submissions, :invitation_token
   end
 end
