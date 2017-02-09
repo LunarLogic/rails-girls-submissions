4b3e3762aa83ada3d41105b92222a567c8014bf2
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-09 12:07:26 +0100

    Make integration test

--- spec/features/inviting_new_submissions_in_place_of_expired_ones_feature_spec.rb
+++ spec/features/inviting_new_submissions_in_place_of_expired_ones_feature_spec.rb
@@ -5,11 +5,20 @@
 require 'rails_helper'
 require 'rake'
 
-describe 'scheduled:invite_new_submissions_in_place_of_expired_ones' do
+describe 'inviting new submissions in place of expired ones' do
   let(:rake)      { Rake::Application.new }
-  let(:task_name) { self.class.top_level_description }
+  let(:task_name) { 'scheduled:invite_new_submissions_in_place_of_expired_ones' }
   let(:task_path) { "lib/tasks/scheduled/invite_new_submissions_in_place_of_expired_ones" }
-  let(:submisssions_inviter) { instance_double SubmissionsInviter }
+  let(:setting) { FactoryGirl.build(:setting, available_spots: 3) }
+  let!(:to_invite_submission) do
+    FactoryGirl.create(
+      :submission,
+      :with_rates,
+      invitation_token: nil,
+      invitation_confirmed: false,
+      rates_num: setting.required_rates_num,
+      rates_val: 2)
+  end
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
@@ -21,11 +30,11 @@ describe 'scheduled:invite_new_submissions_in_place_of_expired_ones' do
     Rake.application.rake_require(task_path, [Rails.root.to_s], loaded_files_excluding_current_rake_file)
 
     Rake::Task.define_task(:environment)
+    allow(Setting).to receive(:get).and_return(setting)
   end
 
   it 'calls correct service' do
-    expect(SubmissionsInviter).to receive(:new).and_return(submisssions_inviter)
-    expect(submisssions_inviter).to receive(:call)
     subject.invoke
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
 end
