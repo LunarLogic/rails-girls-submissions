5a950bd98903c4ad7e9e0ba9139500ddd4a82b8d
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-02-13 16:23:15 +0100

    [110] Add AdultRule - submission is rejected for underage applicants

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,7 @@ class Submission < ActiveRecord::Base
   validates :full_name, :age, :email, :codecademy_username, :description, :html, :css, :js, :ror, :db,
             :programming_others, :english, :operating_system, :goals, presence: true
   validates :first_time, inclusion: { in: [true, false] }
-  validates :age, numericality: { greater_than_or_equal_to: 18, less_than: 110 }
+  validates :age, numericality: { greater_than_or_equal_to: 0, less_than: 110 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :email, uniqueness: { case_sensitive: false }
   validates :full_name, :email, :codecademy_username, length: { maximum: 50 }
