03c443ad43c1007528ccf00c5ec04a47c6aa4a3e
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-13 16:39:41 +0100

    Add service for inviting submissions

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,5 +1,7 @@
 class MailingsController < ApplicationController
   def send_invitation_emails
+    SubmissionsInviter.new.call
+
     redirect_to :back, notice: "You have sent the emails."
   end
 end
