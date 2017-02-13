1b43ff316921369858ab802ca2fc8b89259a8706
Arek Kita <arkadiusz.kita@lunarlogic.io>
2017-02-13 16:59:51 +0100

    Make feature spec for confirming invitations

--- spec/features/inviting_accepted_submissions_feature_spec.rb
+++ spec/features/inviting_accepted_submissions_feature_spec.rb
@@ -24,4 +24,60 @@ describe 'inviting accepted submissions' do
     expect(ActionMailer::Base.deliveries.count).to eq(1)
     expect(accepted_submission.reload.invitation_token).not_to be_nil
   end
+
+  it 'confirms submission' do
+    invited_submission = FactoryGirl.create(
+      :submission,
+      :with_rates,
+      invitation_token: 'xxx',
+      invitation_token_created_at: Time.now,
+      invitation_confirmed: false,
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    visit submissions_confirm_invitation_path(invitation_token: invited_submission.invitation_token)
+    expect(invited_submission.reload.invitation_confirmed).to be true
+    expect(page).to have_text('confirmed')
+  end
+
+  it "doesn't confirm submission with expired invitation token" do
+    invited_submission = FactoryGirl.create(
+      :submission,
+      :with_rates,
+      invitation_token: 'zzz',
+      invitation_token_created_at: 1.week.ago - 1,
+      invitation_confirmed: false,
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    visit submissions_confirm_invitation_path(invitation_token: invited_submission.invitation_token)
+    expect(invited_submission.reload.invitation_confirmed).to be false
+    expect(page).to have_text('expired')
+  end
+
+  it 'show correct info when invitation is already confirmed but token expired' do
+    confirmed_submission = FactoryGirl.create(
+      :submission,
+      :with_rates,
+      invitation_token: 'yyy',
+      invitation_token_created_at: 1.week.ago - 1,
+      invitation_confirmed: true,
+      rates_num: setting.required_rates_num,
+      rates_val: 1)
+
+    visit submissions_confirm_invitation_path(invitation_token: confirmed_submission.invitation_token)
+    expect(page).to have_text('confirmed')
+  end
+
+  context 'handling exceptions' do
+    it 'missing invitation token' do
+      visit submissions_confirm_invitation_path
+      expect(page).to have_text('Something went wrong')
+    end
+
+    it 'submission not found' do
+      visit submissions_confirm_invitation_path('uuu')
+      expect(page).to have_text('Something went wrong')
+    end
+  end
 end
