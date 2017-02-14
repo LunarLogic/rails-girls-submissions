9d1e45584807f4917e1dcda45f1eb6b2474e053c
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-02-14 17:12:42 +0100

    [125] Add Selenium driver to Capybara resulting in showing the browser when running integration tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
 require 'capybara-screenshot/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -76,3 +77,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.infer_spec_type_from_file_location!
 end
+
+
+Capybara.register_driver :selenium do |app|
+ Capybara::Selenium::Driver.new(
+   app,
+   browser: :firefox,
+   desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
+ )
+end
+Capybara.default_driver = :selenium
