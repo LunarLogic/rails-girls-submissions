cdfb60e93556ac787ea11b40a0394cba6f184072
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-04-20 16:18:10 +0200

    Fix specs after styling codecademy course completeness buttons

--- spec/features/setting_codecademy_status_spec.rb
+++ spec/features/setting_codecademy_status_spec.rb
@@ -4,6 +4,11 @@ describe "setting CodeCademy course completeness status" do
   let(:submission) { FactoryGirl.create(:submission, codecademy_status: codecademy_status) }
   let(:user) { FactoryGirl.create(:user) }
 
+  let(:completed_text) { "Course completed" }
+  let(:not_completed_text) { "Course not completed" }
+  let(:completed_button) { "Completed" }
+  let(:not_completed_button) { "Not completed" }
+
   before do
     login_as(user, scope: :user)
     visit submission_path(:valid, submission)
@@ -11,36 +16,36 @@ describe "setting CodeCademy course completeness status" do
 
   context "there is a completed status set" do
     let(:codecademy_status) { true }
-    it { expect(page).to have_text("Completed") }
+    it { expect(page).to have_text(completed_text) }
   end
 
   context "there is a incompleted status set" do
     let(:codecademy_status) { false }
-    it { expect(page).to have_text("Incompleted") }
+    it { expect(page).to have_text(not_completed_text) }
   end
 
   context "there is no status set" do
     let(:codecademy_status) { nil }
 
     it "has two buttons instead" do
-      expect(page).not_to have_text("Completed")
-      expect(page).not_to have_text("Incompleted")
-      expect(page).to have_button("Completed")
-      expect(page).to have_button("Incompleted")
+      expect(page).not_to have_text(completed_text)
+      expect(page).not_to have_text(not_completed_text)
+      expect(page).to have_button(completed_button)
+      expect(page).to have_button(not_completed_button)
     end
 
     it "changes status to completed on clicking completed button" do
-      click_button("Completed")
-      expect(page).to have_text("Completed")
-      expect(page).not_to have_button("Completed")
-      expect(page).not_to have_button("Incompleted")
+      click_button(completed_button)
+      expect(page).to have_text(completed_text)
+      expect(page).not_to have_button(completed_button)
+      expect(page).not_to have_button(not_completed_button)
     end
 
     it "changes status to incompleted on clicking incompleted button" do
-      click_button("Incompleted")
-      expect(page).to have_text("Incompleted")
-      expect(page).not_to have_button("Incompleted")
-      expect(page).not_to have_button("Completed")
+      click_button(not_completed_button)
+      expect(page).to have_text(not_completed_text)
+      expect(page).not_to have_button(not_completed_button)
+      expect(page).not_to have_button(completed_button)
     end
   end
 end
